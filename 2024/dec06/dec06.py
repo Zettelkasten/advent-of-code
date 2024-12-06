e20bc767ea9a67203be14064dbfb8c76f035fd58
Frithjof <fmpetrick@gmail.com>
2024-12-06 10:44:54 +0100

    dec 6 more

--- 2024/dec06/dec06.py
+++ 2024/dec06/dec06.py
@@ -21,44 +21,45 @@ tiles_visited = 1
 grid[start_row][start_col] = dir
 
 # simulate ...
-while True:
-    row_dir, col_dir = dirs_to_row_col[dir]
-    start_row += row_dir
-    start_col += col_dir
-    if not (0 <= start_row < num_rows and 0 <= start_col < num_cols):
-        # ran away
-        break
-    else:
-        new_ground = grid[start_row][start_col]
-        if new_ground == "#":
-            # need to turn
-            start_row -= row_dir  # hacky backtrace ..
-            start_col -= col_dir
-
-            # turn
-            dir = dirs_list[(dirs_list.index(dir) + 1) % len(dirs_list)]
-            row_dir, col_dir = dirs_to_row_col[dir]
-            # update tile with new direction for consistency
-            grid[start_row][start_col] = dir
-
-            # turn for realz
-            start_row += row_dir
-            start_col += col_dir
-            new_ground = grid[start_row][start_col]
-            assert new_ground != "#"
-
-        # explicitly no "else" here ...
-        if new_ground in dirs_to_row_col:
-            # was here before, do not count again
-            pass
-        elif new_ground == ".":
-            # go here first time
-            grid[start_row][start_col] = dir
-            tiles_visited += 1
+def simulation(grid, start_row, start_col, dir, can_place_obstacle):
+    while True:
+        row_dir, col_dir = dirs_to_row_col[dir]
+        start_row += row_dir
+        start_col += col_dir
+        if not (0 <= start_row < num_rows and 0 <= start_col < num_cols):
+            # ran away
+            break
         else:
-            assert False, new_ground
+            new_ground = grid[start_row][start_col]
+            if new_ground == "#":
+                # need to turn
+                start_row -= row_dir  # hacky backtrace ..
+                start_col -= col_dir
 
-    print("\n".join("".join(row) for row in grid))
-    print()
+                # turn
+                dir = dirs_list[(dirs_list.index(dir) + 1) % len(dirs_list)]
+                row_dir, col_dir = dirs_to_row_col[dir]
+                # update tile with new direction for consistency
+                grid[start_row][start_col] = dir
+
+                # turn for realz
+                start_row += row_dir
+                start_col += col_dir
+                new_ground = grid[start_row][start_col]
+                assert new_ground != "#"
+
+            # explicitly no "else" here ...
+            if new_ground in dirs_to_row_col:
+                # was here before, do not count again
+                pass
+            elif new_ground == ".":
+                # go here first time
+                grid[start_row][start_col] = dir
+                tiles_visited += 1
+            else:
+                assert False, new_ground
+
+        print("\n".join("".join(row) for row in grid))
+        print()
 
 print(tiles_visited)
