07cabdeb34f8452e42216df0c74e00a53fb32f69
Frithjof Petrick <fpetrick@apptek.com>
2025-12-08 19:15:58 +0100

    2025 dec 8

---
+++ 2025/dec08/a.py
@@ -0,0 +1,28 @@
+import numpy
+from tqdm import tqdm
+coords = numpy.asarray([line.split(",") for line in open("input")], dtype=numpy.float32)  # [B,3]
+
+num_els = len(coords)
+
+clusters = {i: frozenset({i}) for i in range(num_els)}
+
+def dist(i, j):
+    all_dists =[]
+    for ii in clusters[i]:
+        for jj in clusters[j]:
+            this_dist = numpy.linalg.norm(coords[ii] - coords[jj])
+            all_dists.append(this_dist)
+    return min(all_dists)
+
+all_lists = sorted([(i, j) for i in range(num_els) for j in range(num_els) if i < j and i not in clusters[j]], key=lambda x: dist(x[0], x[1]))
+
+for closest_i, closest_j in all_lists[:1000]:
+    new_set = frozenset(clusters[closest_i] | clusters[closest_j])
+    print("Connected", closest_i, closest_j, "aka", coords[closest_i], coords[closest_j], "which ")
+    for el in new_set:
+        clusters[el] = new_set
+
+# find the three largest clusters
+cluster_sizes = [len(cluster) for cluster in set(clusters.values())]
+print("OUT", sorted(cluster_sizes))
+print(numpy.prod(list(sorted(cluster_sizes))[-3:]))
